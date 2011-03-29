4eb599c31c0d53e7302bbafd67089eb1c008e02d
Julia Ferraioli <julia.ferraioli@docusign.com>
2011-03-29 14:04:30 -0700

    Ruby sample initial commit

---
+++ Ruby/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DocuSignSample::Application.config.secret_token = '313236658a5a663eb6b8e9e5c385af8bea9a6c4b0ac7761c83f87e102abdf18c4ec5eb31466666b4c28bcec4be3759cd32015c494f95a1ffd3311dc5301d8094'
